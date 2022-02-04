3bf61e6518c8cbc353cda91297c4cf2fce597059
ShaneWD <DsiShaneW@gmail.com>
2022-02-04 09:31:03 -0500

    uses fake user agent

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
 
 var bot = async function() {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({headless: false, slowMo: 200});
   const page = await browser.newPage();
-  await page.goto('https://github.com/ShaneWD');
-  await page.screenshot({ path: 'hPage.png' });
+  await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
+  await page.goto('https://www.nike.com/launch/t/space-hippie-01-this-is-trash-carbon-green');
+  // await page.screenshot({ path: 'hPage.png' });
 
   await browser.close();
 }();
