c666923c2473e8a3c17c8198e02bae2b888061d6
ShaneWD <DsiShaneW@gmail.com>
2022-02-04 08:46:35 -0500

    uses stealth plugin for better user mimicking

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra')
+// add stealth plugin and use defaults (all evasion techniques)
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
-(async () => {
-  const browser = await puppeteer.launch();
+var bot = async function() {
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto('https://github.com/ShaneWD');
   await page.screenshot({ path: 'hPage.png' });
 
   await browser.close();
-})();
+}();
