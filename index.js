d7b11cf434138bca3939bc3fb1dd88ba174a05f7
ShaneWD <DsiShaneW@gmail.com>
2022-02-22 11:32:09 -0500

    runs on finishline instead of nike snkrs

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const puppeteer = require('puppeteer-extra')
-// add stealth plugin and use defaults (all evasion techniques)
+
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
 
-var bot = async function() {
-  const browser = await puppeteer.launch({headless: false, slowMo: 200});
-  const page = await browser.newPage();
-  await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
-  await page.goto('https://www.nike.com/launch/t/space-hippie-01-this-is-trash-carbon-green');
-  // await page.screenshot({ path: 'hPage.png' });
 
-  await browser.close();
-}();
+puppeteer.launch({ headless: false }).then(async browser => {
+  console.log('Running...')
+  const page = await browser.newPage()
+  await page.goto('https://www.finishline.com/store/product/mens-nike-air-max-90-casual-shoes/prod772616?styleId=DR0145&colorId=002')
+  await page.waitForTimeout(5000)
+})
