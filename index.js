a941c210258600a5df98910f10b9e5f1ddd607ee
ShaneWD <DsiShaneW@gmail.com>
2022-02-25 07:28:51 -0500

    correctly clicks the right shoe size button

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-const puppeteer = require('puppeteer-extra');
+const puppeteer = require('puppeteer-extra')
 
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-puppeteer.use(StealthPlugin());
+// add stealth plugin and use defaults (all evasion techniques)
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
-(async function main(){
-
-  try {
-    puppeteer.launch({ headless: false }).then(async browser => {
-      console.log('Running...')
-      const page = await browser.newPage()
-      await page.goto('https://www.finishline.com/store/product/mens-nike-air-max-90-casual-shoes/prod772616?styleId=DR0145&colorId=002')
-      await page.waitForTimeout(5000)
-    })  
-
-
-
-  } catch (error) {
-      console.log(error)
-  }
-
-})();
+// puppeteer usage as normal
+puppeteer.launch({ headless: false }).then(async browser => {
+  console.log('Running tests..')
+  const page = await browser.newPage()
+  await page.goto('https://www.finishline.com/store/product/mens-nike-air-max-90-casual-shoes/prod772616?styleId=DR0145&colorId=002')
+  await page.waitForTimeout(5000)
+  await page.click('.sizeOptions[data-size="11.0"]')
+  // await page.screenshot({ path: 'testresult.png', fullPage: true })
+  // await browser.close()
+  // console.log(`All done, check the screenshot. ✨`)
+})
